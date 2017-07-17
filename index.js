888981fbec6b737c6f49024868f131deb2d3753e
zhaoshiming <1406554044@qq.com>
2017-07-17 17:45:36 +0800

    yes

---
+++ index.js
@@ -0,0 +1,64 @@
+11日工作总结：
+
+
+12日待完成工作：
+1.	完成angularjs 教程部分，完成阅读部分的阅读总结。
+2.	完成当日bug修复，开始项目js文件梳理，并且保留完成梳理的文件名称，计算进度。完成梳理总结。
+3.	继续js权威指南阅读，完成第二章的阅读，完成第二章阅读总结。
+4．完成莲都 马鞍山 曲靖 首页的导航跳转
+5. 完成git服务的处理
+
+12 日完成工作：
+1.	完成三个微信端跳转切换
+2.	完成bug修复，项目梳理
+3.	Git part 50%
+
+
+项目总结： angular项目总体文件目录格式为 
+1. Css    2. Js    3. Img     4.lib     5 .index,html  6. Test file  7. Json file 
+Js part contains:  controllers and recapp( start js ) and routeapp , directives（指令） 
+ service (used to get data from back server)     filters  commonjs (all can be used function)
+
+12日工作总结：
+	未完成angularjs教程 未完成js权威指南阅读，未完成git全部内容
+
+13日待完成工作
+	完成angularjs教程阅读总结
+	完成js权威指南第二章阅读总结
+	完成git全部内容
+	完成马鞍山首页布局和ps
+
+13 日完成工作
+	完成马鞍山首页布局和ps（采取的是直接样式）
+
+13日工作总结
+	无？？？？？？？？？？？
+
+14日待完成工作
+	完成angularjs教程阅读
+完成js权威指南第二章阅读总结
+	完成git版本库测试
+	开始angular2 教程阅读，完成阅读总结
+
+
+15 日 null
+
+16日 完成js权威指南到第四章了，完成h5 、css3部分阅读，pwa也是基于localstorage和本地数据库的基础的，很是值得深入，然后fuck react!
+
+17日待完成任务
+	完成angularjs教程阅读 emergency
+	完成js权威指南第五章阅读  emergency
+	完成git版本库测试， 完成文档的编写 今天完成
+	开始angular2教程阅读 emergency
+	第三方登陆测试 important
+	完成三端首端导航修改
+
+17日完成任务
+	完成三端首端导航修改
+	完成angular2部分教程阅读，现在到service部分
+	
+
+17日工作总结
+	Angular2的组件化开发，main.js为入口文件，会设置初始化是一个组件，这个组件其实可以作为根部组件，在这个组件里，嵌套其他的组件。
+	所以的组件都需要在app.module.ts里面进行注册。
+	组件里面会通过导入component ，把当前页面全部的资源引入，在父组件向子组件传值的时候，子组件里面的属性需要设置为@input 
